d38531c3e532b587a2ba37da88916eaa1cacda2e
adriel45dev <45adrielalves45@gmail.com>
2023-10-24 10:21:37 -0300

    -- narow error api --

---
+++ pages/api/getAllUsers.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, ROLE } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  "use server";
+
+  if (req.method === "GET") {
+    const guests = await prisma.guest.findMany();
+
+    return res.status(200).json({ guests });
+  }
+}
